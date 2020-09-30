c02432071927bf32f7fefb15546ec688434a1944
Facundo Montero <facumo.fm@gmail.com>
2020-09-30 14:20:23 -0300

    neolightning: PHPMaterialImageGenerator: drop for now.

--- services/getRandomImage.php
+++
@@ -1,9 +0,0 @@
-<?php
-chdir('..');
-
-require_once 'includes/settings.php';
-$width = 1600; $height = 900;
-$availableColors = SHARED_VALUES['THEME'];
-$limit = 4;
-require_once 'vendor/neolightning/PHPMaterialImageGenerator/MaterialImageGen.php';
-?>
