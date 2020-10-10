db0aec8c96060a70d1a9bb0f0ef2079ab6beb8bc
FacuM <facumo.fm@gmail.com>
2020-10-10 16:57:00 -0300

    database: disable ordered array render.

--- includes/database.php
+++ includes/database.php
@@ -7,7 +7,8 @@ try {
             'dbname='   . DATABASE['name']     . ';' . 
             'charset='  . DATABASE['encoding'], 
         DATABASE['username'], 
-        DATABASE['password']
+        DATABASE['password'],
+        [ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ]
     );
 } catch (PDOException $exception) {
     header('location: ' . SHARED_VALUES['SYSTEM_HOSTNAME'] . 'exceptions/maintenance');
