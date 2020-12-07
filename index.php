4756f5fc05ec737d2a9b1e66de030dfb9a436342
Facundo Montero <facumo.fm@gmail.com>
2020-12-07 04:14:53 -0300

    footer, index: force assets update.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-$title = 'Página principal'; $js = 'index.min.js?v=4';
+$title = 'Página principal'; $js = 'index.min.js?v=5';
 
 require_once 'includes/main.php'; 
 require_once 'views/header.php';
