48611d431de8c410bf12189d5717fef2c6199f76
Facundo Montero <facumo.fm@gmail.com>
2021-01-05 23:03:19 -0300

    assets: force update.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-$title = 'Página principal'; $js = 'index.min.js?v=11';
+$title = 'Página principal'; $js = 'index.min.js?v=12';
 
 require_once 'includes/main.php'; 
 require_once 'views/header.php';
