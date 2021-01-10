37ae6373a0bc7f46d210551271e32a88fb48440f
Facundo Montero <facumo.fm@gmail.com>
2021-01-10 20:20:06 -0300

    common: make the chunk puller public and let it be aborted.

--- assets/js/common.js
+++ assets/js/common.js
@@ -33,6 +33,8 @@ let gotHistoryPushState = false;
 
 let userNavBackground = null;
 
+let chunkPuller = null;
+
 const FONTS = [
     {
         family:  'Lato',
@@ -1014,13 +1016,17 @@ async function tryToPullChunks(firstCall = false, then = () => {}) {
             if (isPullingChunks) {
                 console.info('tryToPullChunks: cannot pull now, there\'s a pending request.');
             } else {
+                if (chunkPuller != null) {
+                    chunkPuller.abort();
+                }
+
                 lastMessageData = getLastMessageData();
 
                 gridContainer = $('.gridContainer');
 
                 preloader = gridContainer.find('.preloader-container');
  
-                run('messagesManager', 'getRecent', {
+                chunkPuller = run('messagesManager', 'getRecent', {
                     after:      typeof(lastMessageData) != 'undefined' ? lastMessageData.message  : undefined,
                     startAt:    typeof(lastMessageData) != 'undefined' ? lastMessageData.position : undefined,
                     recipient:  RECIPIENT,
@@ -1105,6 +1111,9 @@ async function tryToPullChunks(firstCall = false, then = () => {}) {
                     isPullingChunks = false;
 
                     console.error(error);
+                })
+                .always(() => {
+                    chunkPuller = null;
                 });
             }
         } else {
